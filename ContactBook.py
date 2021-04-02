2fccc1385db4c1583581f73e4b125c3f53dca3a2
Dave Easley <easley.david@gmail.com>
2021-04-02 14:39:05 -0400

    added a contact

--- ContactBook.py
+++ ContactBook.py
@@ -1,20 +1,23 @@
-from peewee import *
+from peewee import CharField, IntegerField, Model, PostgresqlDatabase, TextField
 
 db = PostgresqlDatabase('contact', user= 'davideasley', password= '', host='localhost', port=5432)
 
 db.connect()
 
+
 class BaseModel(Model):
     class Meta:
         database = db
 
+
 class Contact(BaseModel):
     name = CharField()
-    phone = IntegerField()
-    email= CharField()
+    phone = CharField()
+    email = CharField()
+
 
 db.create_tables([Contact])
 
-mom = Contact.get(Contact.name == "Mom", Contact.phone == "5551111234", Contact.email == "mom@mommail.com")
+mom = Contact(name = 'Mom', phone = '1234567890', email = 'mom@mommail.com')
 print(mom.name, mom.phone, mom.email)
 mom.save()
