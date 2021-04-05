264203f95945c280c5467563f419eb5b09d1a87a
Dave Easley <easley.david@gmail.com>
2021-04-05 11:49:40 -0400

    added delete

--- ContactBook.py
+++ ContactBook.py
@@ -42,8 +42,9 @@ def search_contacts(contacts):
     if search_answer == 'y':
         search_name = str(
             input('Which first name would you like to search for?: ')).lower()
-        result = contacts.where(Contact.first_name== search_name) ##doesn't work right
-        print(result)
+        result = contacts.select().where(Contact.first_name == search_name) 
+        for contact in result:
+            print(f'Full Name: {contact.first_name} {contact.last_name}, Phone: {contact.phone}, Email: {contact.email}')
     else:
       contact_add(contacts)
 
@@ -55,18 +56,33 @@ def contact_add(contacts):
         new_phone = str(input('What is the new phone number?: ' ))
         new_email = str(input('What is the new email address?: ' )).lower()
         new_contact = Contact(first_name = new_first_name, last_name= new_last_name, phone = new_phone, email = new_email ).save()
-        print(new_contact)
+        for contact in contacts:
+             print(f'Full Name: {contact.first_name} {contact.last_name}, Phone: {contact.phone}, Email: {contact.email}')
+
+    else:
+       delete_contact(contacts)
 
 
 # def update_contacts(contacts):
 #     update_question = str(input('Would you like to edit a contact? y or n: ')).lower()  
 #     if update_question == 'y':
-#         update_person = str(input('Which contact would you like to contact by name?: '))
-#         new_name = str(input('What is the new name?: '))
-#         new_phone = str(input('What is the new phone number?: '))
-#         new_email = str(input('What is the new email?: '))
+#         update_person = str(input('Which contact would you like to contact by first name?: '))
+#         new_first_name = str(input('What is the new first name?: ' )).lower()
+#         new_last_name = str(input('What is the new last name?: ' )).lower()
+#         new_phone = str(input('What is the new phone number?: ' ))
+#         new_email = str(input('What is the new email address?: ' )).lower()
+#         result = contacts.where(Contact.first_name == update_person)
+#         result = Contact.update(first_name = new_first_name, last_name= new_last_name, phone = new_phone, email = new_email )
+#     else:
+#       delete_contact(contacts)
 
+def delete_contact(contacts):
+    update_question = str(input('Would you like to delete a contact? y or n: ')).lower()  
+    if update_question == 'y':
+       update_person = str(input('Which contact would you like to delete by first name?: '))
+       result = Contact.delete().where(Contact.first_name == update_person).execute()
 
-
+    
+ 
 
 all_contacts(want_all_answer)
